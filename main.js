6712ce70eaaf54622a4f4518a35c724ad3adbe03
ohmydarlingclementine <linaorundas70@gmail.com>
2025-12-18 04:26:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,70 @@ let player;
 let platforms = [];
 let score = 0;
 let game_state = "start";
+
+// physical main settings
+const GRAVITY = 0.4;
+const JUMP_FORCE = -10;
+const MOVE_SPEED = 5;
+
+// the size of the platform
+const PLATFORM_WIDTH = 80;
+const PLATFORM_HEIGHT = 15;
+
+// the edges of the platform
+const EDGE_MARGIN = 10;
+
+// the vertical space between the platforms
+const GAP_MIN = 60;
+const GAP_MAX = 115;
+
+// the number of platform creating and testing
+const TRY_COUNT = 12;
+
+// the chanche for another bigger gap
+const BIG_GAP_CHANCE = 0.12;
+
+function setup() {
+  createCanvas(600, 800); // make the screen 600x800 
+  reset_game();
+}
+
+function draw() {
+  background(220);
+
+  
+  // check the state of the game w this (start: started, gameover: ended)
+  if (game_state === "start") return draw_start_screen();
+  if (game_state === "gameover") return draw_game_over_screen();
+
+  play_game();
+}
+
+function keyPressed() { // Space / Enter / R / r when u press these
+  if (game_state === "start" && (keyCode === ENTER || key === " ")) {
+    game_state = "playing";
+    reset_game();
+  }
+
+  if (game_state === "gameover" && (key === "r" || key === "R" || key === " ")) {
+    game_state = "playing";
+    reset_game();
+  }
+}
+
+// main screen 
+function draw_start_screen() {
+  textAlign(CENTER);
+  fill(0);
+
+  textSize(32);
+  text("Escape the Tower", width / 2, height / 2 - 40);
+
+  textSize(16);
+  text("To start press ENTER or SPACE", width / 2, height / 2 + 10);
+  text("To move you can use ARROW KEYS or A/D ", width / 2, height / 2 + 40);
+
+  // the color description (for player to understand)
+  text("Green: stable  |  Blue: moves right and left  |  Yellow: moves up and down", width / 2, height / 2 + 95);
+  text("Red: fragile (it breaks when you press it)", width / 2, height / 2 + 125);
+}
